8b5ce787b4ad6be512de54004febdda5222635d8
Axel Tifrani <axeltifrani@gmail.com>
2024-08-23 16:28:58 +0200

    deploy aws public subnet  resource

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-resource "aws_vpc" "dev_main" {
+resource "aws_vpc" "dev_vpc" {
   cidr_block = "10.172.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
     Name = "dev"
+  }
+}
+
+resource "aws_subnet" "dev_public_subnet" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.172.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "dev-public"
   }
 }
