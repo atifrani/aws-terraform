0917c6bda824155a78e7c925c416eba6040889a0
Axel Tifrani <axeltifrani@gmail.com>
2024-08-23 16:34:05 +0200

    deploy aws internet gateway  resource

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.172.0.0/16"
+  cidr_block           = "10.172.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev"
@@ -9,12 +9,20 @@ resource "aws_vpc" "dev_vpc" {
 }
 
 resource "aws_subnet" "dev_public_subnet" {
-  vpc_id     = aws_vpc.dev_vpc.id
-  cidr_block = "10.172.1.0/24"
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.172.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "eu-west-1a"
+  availability_zone       = "eu-west-1a"
 
   tags = {
     Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "dev_igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "dev_igw"
   }
 }
